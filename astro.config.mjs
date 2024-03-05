b88d409d6c9a5f6db065ba372d123d8ad246c556
Lucas Vogel <contact@vogelino.com>
2024-03-05 11:37:55 -0300

    chore: Add BMBF_Logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [
+    tailwind(),
+    mdx(),
+    icon({
+      iconDir: "src/assets/icons",
+    }),
+  ],
 });
