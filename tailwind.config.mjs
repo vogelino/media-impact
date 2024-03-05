9f64b55d0c5f9027e78c8cff2f02bfbeb3b9b7cd
Lucas Vogel <contact@vogelino.com>
2024-03-04 23:38:16 -0300

    feat(ci): Add logo and CI

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,23 @@
+import colors from "tailwindcss/colors.js";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        headlines: ["Neue Regrade", "sans-serif"],
+      },
+      colors: {
+        fg: "var(--fg)",
+        bg: "var(--bg)",
+        bgOverlay: "var(--bgOverlay)",
+        alt: "var(--alt)",
+        grayDark: "var(--grayDark)",
+        grayMed: "var(--grayMed)",
+        grayLight: "var(--grayLight)",
+        grayUltraLight: "var(--grayUltraLight)",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
